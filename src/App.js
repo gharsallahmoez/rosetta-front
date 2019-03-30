53eb9b49d09cd95c3ec9219d8ec06edd821e397f
gharsallahmoez <gharsallahmoez@gmail.com>
2019-03-30 14:11:33 +0100

    initialize project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Dashboard from './components/Dashboard'
+import Header from "./components/Layout/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {BrowserRouter as Router, Route } from "react-router-dom";
+import AddCollection from "./components/Project/addCollection";
+import {Provider} from 'react-redux';
+import store from "./store";
+import UpdateCollection from "./components/Project/updateCollection";
+import CollectionBoard from "./components/projectBoard/CollectionBoard"
+import AddShoe from "./components/projectBoard/ProjectTasks/AddShoe";
+import UpdateShoe from "./components/projectBoard/ProjectTasks/UpdateShoe";
 
 class App extends Component {
-  render() {
+    render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    <Provider store={store}>
+    <Router>
+    <div className="App">
+    <Header />
+    <Route exact path={"/dashboard"}  component={Dashboard}/>
+    <Route exact path={"/addCollection"}  component={AddCollection}/>
+    <Route exact path={"/updateCollection/:id"}  component={UpdateCollection}/>
+    <Route exact path={"/collectionBoard/:id"}  component={CollectionBoard}/>
+    <Route exact path={"/addShoe/:id"}  component={AddShoe}/>
+    <Route exact path={"/updateShoe/:backlog_id/:pt_id"}  component={UpdateShoe}/>
+    </div>
+    </Router>
+    </Provider>
     );
   }
 }
