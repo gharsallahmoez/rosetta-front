2623a01257624382b2df9b2fa3cc8587e2e846cf
gharsallahmoez <gharsallahmoez@gmail.com>
2019-03-31 00:25:21 +0100

    export axios url

--- src/actions/collectionActions.js
+++ src/actions/collectionActions.js
@@ -1,10 +1,11 @@
 import axios from 'axios'
 import {GET_ERRORS, GET_COLLECTION, GET_COLLECTIONS , DELETE_COLLECTION} from "./types";
+import {axiosInstance_baseURL} from "../webConfig"
 
 
 export const createCollection = (collection, history) => async dispatch => {
     try {
-        await axios.post("https://rosetta.cfapps.io/api/collection",collection)
+        await axios.post(`${axiosInstance_baseURL}/api/collection`,collection)
         history.push("/dashboard");
         dispatch({
             type:GET_ERRORS,
@@ -19,7 +20,7 @@ export const createCollection = (collection, history) => async dispatch => {
 }
 
 export const getCollections = () => async dispatch => {
-    const res = await axios.get("https://rosetta.cfapps.io/api/collection/all");
+    const res = await axios.get(`${axiosInstance_baseURL}/api/collection/all`);
     dispatch({
         type: GET_COLLECTIONS,
         payload: res.data
@@ -29,7 +30,7 @@ export const getCollections = () => async dispatch => {
 
 export const getCollection = (id, history)  => async dispatch => {
     try {
-        const res = await axios.get(`https://rosetta.cfapps.io/api/collection/${id}`);
+        const res = await axios.get(`${axiosInstance_baseURL}/api/collection/${id}`);
         dispatch({
             type: GET_COLLECTION,
             payload: res.data
@@ -41,7 +42,7 @@ export const getCollection = (id, history)  => async dispatch => {
     export const deleteCollection = id => async dispatch =>{
     if(window.confirm("vous ete sure de supprimer cette collection ?"))
     {
-        await axios.delete(`https://rosetta.cfapps.io/api/collection/${id}`);
+        await axios.delete(`${axiosInstance_baseURL}/api/collection/${id}`);
         dispatch({
             type : DELETE_COLLECTION,
             payload : id
